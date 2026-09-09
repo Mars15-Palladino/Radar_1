010f2c38d138d682695114d503ff0c50e6e32f4d
Mateus H. De Lima Barreto <henriquebarreto945@gmail.com>
2026-09-09 16:10:22 -0300

    TESTES PARA TELA
    APRENDENDOA USAR O SPI

--- lib/RadarInterfaces/Interface_Fisica.cpp
+++ lib/RadarInterfaces/Interface_Fisica.cpp
@@ -5,14 +5,34 @@ Interface_Fisica_Modulo::Interface_Fisica_Modulo():tela_TFT(TFT_Pino_CS, TFT_Pin
 }
 
 void Interface_Fisica_Modulo::inicializar_tela(){
-    tela_TFT.initR(INITR_BLACKTAB);
-    tela_TFT.fillScreen(ST77XX_BLACK);
+    tela_TFT.initR(INITR_BLACKTAB);// Inicializa o display com a configuração de cor vermelha (INITR_REDTAB) para o ST7735, que define a ordem dos pinos e a forma como os dados são enviados para o display.
 
-    tela_TFT.setTextColor(ST77XX_WHITE);
-    tela_TFT.setTextSize(3);
-    tela_TFT.setCursor(20, 60);
-    tela_TFT.print("TFT OK");
+    tela_TFT.setRotation(1);// Define a rotação da tela para 1, que é a orientação paisagem (landscape) do display. Isso significa que o lado mais longo do display estará na horizontal e o lado mais curto estará na vertical. A rotação é importante para garantir que os elementos gráficos sejam desenhados corretamente na tela. Aceita valores de 0 a 3, onde cada valor representa uma rotação diferente (0 = retrato, 1 = paisagem, 2 = retrato invertido, 3 = paisagem invertida).
 
-    Serial.println("TFT inicializado.");
+    tela_TFT.fillScreen(ST77XX_BLACK);// Limpa a tela do display, preenchendo-a com a cor preta (ST77XX_BLACK), que é definida na biblioteca Adafruit_ST7735.h. Isso garante que a tela esteja limpa antes de desenhar qualquer coisa nela.
+
+    tela_TFT.setTextColor(ST77XX_YELLOW);// Define a cor do texto
+    tela_TFT.setTextSize(1);// Define o tamanho do texto, ocupando 6 pixel de largura e 8 pixel de altura por caractere. O tamanho do texto pode ser ajustado para valores maiores, como 2, 3, etc., para aumentar a legibilidade do texto na tela.
+
+    tela_TFT.setCursor(40, 2);// Define a posição do cursor para o texto
+    tela_TFT.print("Radar");
+
+    tela_TFT.setTextColor(ST77XX_WHITE);// Define a cor do texto
+    tela_TFT.setTextSize(1);// Define o tamanho do texto
+    tela_TFT.setCursor(40, 40);// Define a posição do cursor para o texto
+    tela_TFT.print("Centro");
+
+
+    tela_TFT.setTextColor(ST77XX_WHITE);// Define a cor do texto
+    tela_TFT.setTextSize(1);// Define o tamanho do texto
+    tela_TFT.setCursor(40, 50);// Define a posição do cursor para o texto
+    tela_TFT.print("03/09/2026");
+
+
+   delay(2000);// Aguarda 2 segundos para que o usuário possa ver a tela inicial antes de prosseguir com o restante do código. Isso é útil para exibir informações importantes, como o nome do projeto, a data ou outras mensagens iniciais, antes de iniciar a operação principal do radar.
+
+   tela_TFT.fillRect(35, 35, 120, 52, ST77XX_RED);// Limpa a tela do display novamente, preenchendo-a com a cor preta (ST77XX_BLACK). Isso é feito para remover a tela inicial e preparar o display para exibir as informações principais do radar.
+
+
 
 }
--- lib/RadarInterfaces/monitorSPI.md
+++ lib/RadarInterfaces/monitorSPI.md
@@ -56,3 +56,65 @@ RESET	Reinicia o controlador
 VCC	Alimentação
 GND	Referência
 LED	Iluminação
+
+
+# Sobre  as fontes
+   Aparentemente, elas possuem escalas, com o tamanho sendo de 0-3, com a fonte padrão tendo aproximadamente 6 pixels de largura e 8 de altura por linha
+
+   Radar = 5*6 = 30 pixels
+
+   Se eu mudar para o tamaho 2, a escla dobra
+
+   Boa pergunta. Esse 8 veio da dimensão da fonte padrão do Adafruit_GFX, não de uma regra do seu TFT.
+
+Na fonte padrão, cada caractere ocupa uma célula de aproximadamente:
+
+
+6 × 8 pixels
+↑   ↑
+X   Y
+
+   Resumindo, Definem quato espaço vai ser pego na tela
+
+
+   há toda uma quetsão de cordenadas que vão definir a posição da escrita/desenho
+
+   tela_TFT.setTextColor(ST77XX_YELLOW);// Define a cor do texto
+    tela_TFT.setTextSize(1);// Define o tamanho do texto
+    tela_TFT.setCursor(90, 2);// Define a posição do cursor para o texto
+    tela_TFT.print("Radar");
+
+    tela_TFT.setTextColor(ST77XX_WHITE);// Define a cor do texto
+    tela_TFT.setTextSize(1);// Define o tamanho do texto
+    tela_TFT.setCursor(80, 64);// Define a posição do cursor para o texto
+    tela_TFT.print("Centro");
+
+
+    tela_TFT.setTextColor(ST77XX_WHITE);// Define a cor do texto
+    tela_TFT.setTextSize(1);// Define o tamanho do texto
+    tela_TFT.setCursor(40, 66);// Define a posição do cursor para o texto
+    tela_TFT.print("03/09/2026");
+
+    Acima, tem algo escrito em uma parte da tela, e dois se sobrepondo
+
+    tela_TFT.setTextSize(1);
+tela_TFT.setCursor(40, 40);
+tela_TFT.print("Centro");
+
+Com a fonte padrão e setTextSize(1), podemos considerar aproximadamente:
+
+cada caractere → 6 px de largura
+altura → 8 px
+
+Então "Centro" tem 6 caracteres:
+
+6 × 6 = 36 px de largura
+
+e aproximadamente 8 px de altura.
+
+Logo, a região ocupada seria aproximadamente:
+
+x = 40
+y = 40
+largura ≈ 36
+altura ≈ 8
